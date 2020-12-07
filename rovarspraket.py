2ba8a206c419d921f01fc4cb17c5b7ca7c5435c6
unknown <simomelli2005@gmail.com>
2020-12-07 15:26:39 +0100

    correggo es 3 affinchè l'utente possa fare subito un'altra parola/frase

--- rovarspraket.py
+++ rovarspraket.py
@@ -1,24 +1,26 @@
 input("ES 3")
 
 input("Rovarspraket")
-parola = input("Inserisci in minuscolo la parola che vuoi tradurre in rovarspraket: ")
-count1 = 0
-count2 = -1
-listpar = []
 while True:
-    if count1 == len(parola):
-        break
-    else:
-        lett = parola[count2+1]
-        listpar.append(lett)
-        if lett == "a" or lett == "e" or lett == "i" or lett == "o" or lett == "u":
-            print()
+    parola = input("Inserisci in minuscolo la parola/frase che vuoi tradurre in rovarspraket: ")
+    count1 = 0
+    count2 = -1
+    listpar = []
+    while True:
+        if count1 == len(parola):
+            break
         else:
-            listpar.append("o")
-            listpar.append(lett)        
-        count1 += 1
-        count2 += 1
-stringa = ""
-for x in listpar:
-    stringa += str(x)
-print(stringa)
+            lett = parola[count2+1]
+            listpar.append(lett)
+            if lett != "a" and lett != "e" and lett != "i" and lett != "o" and lett != "u":
+                listpar.append("o")
+                listpar.append(lett)              
+            count1 += 1
+            count2 += 1
+    stringa = ""
+    for x in listpar:
+        stringa += str(x)
+    print(stringa)
+    remake = input("Vuoi provare con un'altra parola/frase? ")
+    if remake == "no" or remake == "NO" or remake == "No":
+        break
