3cc8d590237f1aef92cb2126c0dc906b4b2de50d
studente <studente@Licspalla.local>
2020-12-07 11:56:30 +0100

    svolgo es 3

---
+++ rovarspraket.py
@@ -0,0 +1,26 @@
+input("ES 3")
+
+input("Rovarspraket")
+parola = input("Inserisci in minuscolo la parola che vuoi tradurre in rovarspraket: ")
+count1 = 0
+count2 = -1
+listpar = []
+while True:
+    if count1 == len(parola):
+        break
+    else:
+        lett = parola[count2+1]
+        listpar.append(lett)
+        if lett == "a" or lett == "e" or lett == "i" or lett == "o" or lett == "u":
+            print()
+        else:
+            listpar.append("o")
+            listpar.append(lett)        
+        count1 += 1
+        count2 += 1
+print(listpar)
+stringa = ""
+for x in listpar:
+    str(x)
+    stringa = stringa, x
+print(stringa)
