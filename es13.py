07f2629c24f95102fbc6c71b059e71b7efe8c9b3
unknown <simomelli2005@gmail.com>
2020-12-28 16:08:22 +0100

    svolgo es 13 su numeri pari e dispari a casa

---
+++ es13.py
@@ -0,0 +1,10 @@
+input("ES 13")
+print()
+input("Verifica se un numero è pari o dispari")
+print()
+print("Questo programma riconosce se un numero è pari o dispari")
+n = int(input("Digita un numero: "))
+if n % 2 == 0:
+    print("il numero", n, "è pari")
+else:
+    print("il numero", n, "è dispari")
