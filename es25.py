8cc6ad5ebfc1c91b201c7338e103ca3d67f35faa
unknown <simomelli2005@gmail.com>
2021-02-04 16:37:52 +0100

    svolgo es 25 2a parte su matricole e voti a casa

--- es25.py
+++ es25.py
@@ -5,20 +5,25 @@ input("ES 25")
 #e successivamente visualizza quali voti tra loro sono stati assegnati, riducendo a uno i voti assegnati.
 count = 1
 dict_voti = {}
-list_voti = []
-dict_altro = {}
 while True:
-    if count == 31:
+    if count == 6:
         break
-    voto = int(input("Inserisci il voto dello studente: "))
+    print("Inserisci il voto della matricola", count)
+    voto = int(input("Voto: "))
     if voto > 10:
         print("Il voto massimo che puoi inserire è 10! riprova.")
-        voto = int(input("Inserisci il voto dello studente: "))
-    list_voti.append(voto)
-    dict_altro[voto] = count
+        voto = int(input("Voto: "))
     dict_voti[count] = voto
     count += 1
+list_voti = list(dict_voti.values())
 list_voti.sort()
 print("Questo è l'elenco in ordine crescente di voto")
 for i in list_voti:
-    print("Matricola", dict_altro[i], ":", i)
+    print("Voto", i)
+list_voti = []
+for i in dict_voti:
+    if dict_voti[i] not in list_voti:
+        list_voti.append(dict_voti[i])
+print("I voti assegnati sono i seguenti: ")
+for i in list_voti:
+    print(i)
