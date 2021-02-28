ef427a5674baa5afe176b9208aaa494196bc1bd0
unknown <simomelli2005@gmail.com>
2021-02-28 16:06:23 +0100

    correggo es 25 a casa

--- es25-classi.py
+++ es25-classi.py
@@ -11,30 +11,26 @@ class Triangolo():
 class TriangoloIsoscele(Triangolo):
     def __init__(self, side_1, side_2, side_3):
         super().__init__(side_1, side_2, side_3)
-        self.side_1 = side_1
-        self.side_2 = side_2
-        self.side_3 = side_3
     def info_i(self):
         print("Il triangolo è isoscele")
         print("Il perimetro del triangolo è", self.side_1+self.side_2+self.side_3)
 class TriangoloEquilatero(TriangoloIsoscele):
     def __init__(self, side_1, side_2, side_3):
         super().__init__(side_1, side_2, side_3)
-        self.side_1 = side_1
-        self.side_2 = side_2
-        self.side_3 = side_3
     def info_e(self):
-        print("Il triangolo è isoscele")
+        print("Il triangolo è equilatero")
         print("Il perimetro del triangolo è", self.side_1*3)
 def main():
     side_1 = int(input("Inserisci la misura del primo lato: "))
     side_2 = int(input("Inserisci la misura del secondo lato: "))
     side_3 = int(input("Inserisci la misura del terzo lato: "))
-    t1 = TriangoloEquilatero(side_1, side_2, side_3)
     if side_1 != side_2 and side_1 != side_3 and side_2 != side_3:
+        t1 = Triangolo(side_1, side_2, side_3)
         t1.info_s()
     elif side_1 == side_2 and side_1 == side_3 and side_2 == side_3:
+        t1 = TriangoloEquilatero(side_1, side_2, side_3)
         t1.info_e()
     else:
+        t1 = TriangoloIsoscele(side_1, side_2, side_3)
         t1.info_i()
 main()
