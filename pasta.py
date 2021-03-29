b99923fb292f18e987b548ce31d7d821ac369053
Simone Melli <66493018+SIMELLI25@users.noreply.github.com>
2021-03-29 12:44:57 +0200

    Add files via upload

--- pasta.py
+++ pasta.py
@@ -71,15 +71,68 @@ print(es)
 print(es.dati)
 somma = 0
 for i in es.dati:
-    if es.dati.index(i) % 2 != 0:
+    if i % 2 != 0:
         somma += i
 es.consegna(somma)
-incompleto
 '''
 '''
 es = Verifica.inizia_esercizio(10)
 print(es)
 print(es.dati)
-es.consegna(es.dati.sort())
-incomp0leto
+es.consegna(sorted(es.dati))
+'''
+'''
+es = Verifica.inizia_esercizio(11)
+print(es)
+print(es.dati)
+lista = []
+for i in es.dati:
+    e = i.lower()
+    lista.append(e)
+es.consegna(sorted(lista))
+'''
+'''
+es = Verifica.inizia_esercizio(12)
+print(es)
+print(es.dati)
+lista = []
+for i in es.dati:
+    e = i-1
+    lista.append(e)
+es.consegna(lista)
+'''
+'''
+es = Verifica.inizia_esercizio(13)
+print(es)
+print(es.dati)
+lista = []
+for i in es.dati:
+    if es.dati[i] == len(es.dati)-1:
+        e = i
+    else:
+        e = i + es.dati[es.dati.index(i)+1]
+    lista.append(e)
+print(lista)
+incompleto
+'''
+
+'''
+es = Verifica.inizia_esercizio(14)
+print(es)
+print(es.dati)
+diz = {}
+count_zeri = 0
+count_pos = 0
+count_neg = 0
+for i in es.dati:
+    if i == 0:
+        count_zeri += 1
+    elif i > 0:
+        count_pos += 1
+    elif i < 0:
+        count_neg += 1
+diz["zeri"] = count_zeri
+diz["positivi"] = count_pos
+diz["negativi"] = count_neg
+es.consegna(diz)
 '''
